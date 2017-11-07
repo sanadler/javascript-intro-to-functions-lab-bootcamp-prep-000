9d3a995482a024e8384d98d345ff94c68e2e896e
Sarah Nadler <sarah.m.nadler@gmail.com>
2017-11-07 01:20:16 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -15,7 +15,7 @@ function logWhisper(string){
 }
 
 function sayHiToGrandma(string){
-  if(string === string.toLowerCase){
-    console.log(string);
+  if(string === string.toLowerCase()){
+    return "I can\'t hear you!"
   }
 }
